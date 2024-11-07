0055a656182717ffdffb5f7b09bc7cffc07817bc
matszx <coding@mcygan.anonaddy.me>
2024-11-07 15:36:23 +0100

    clearer text style

--- 01/ex05/src/Harl.cpp
+++ 01/ex05/src/Harl.cpp
@@ -6,22 +6,22 @@ Harl::~Harl(void) {}
 
 void	Harl::_debug(void)
 {
-	std::cout << "Harl: \"I love having extra bacon!\"" << std::endl;
+	std::cout << "Harl: \x1b[3m\"I love having extra bacon!\"\x1b[0m" << std::endl;
 }
 
 void	Harl::_info(void)
 {
-	std::cout << "Harl: \"You didn't put enough bacon in my burger!\"" << std::endl;
+	std::cout << "Harl: \x1b[3m\"You didn't put enough bacon in my burger!\"\x1b[0m" << std::endl;
 }
 
 void	Harl::_warning(void)
 {
-	std::cout << "Harl: \"I deserve to have some extra bacon for free.\"" << std::endl;
+	std::cout << "Harl: \x1b[3m\"I deserve to have some extra bacon for free.\"\x1b[0m" << std::endl;
 }
 
 void	Harl::_error(void)
 {
-	std::cout << "Harl: \"I want to speak to the manager.\"" << std::endl;
+	std::cout << "Harl: \x1b[3m\"I want to speak to the manager.\"\x1b[0m" << std::endl;
 }
 
 void	Harl::complain(std::string level)
