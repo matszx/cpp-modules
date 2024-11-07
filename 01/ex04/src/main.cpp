ba4d5ba135db225c7df02bc938f56dbde1c3a923
matszx <coding@mcygan.anonaddy.me>
2024-11-07 11:43:13 +0100

    ex04 done

---
+++ 01/ex04/src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+
+int	main(int ac, char **av)
+{
+	if (ac != 4 || !*av[1] || !*av[2])
+		return (std::cout << "error: \x1b[3m./replace [file] [s1] [s2]\x1b[0m" << std::endl, 1);
+	
+	std::string		s1 = av[2];
+	std::string 	s2 = av[3];
+	std::ifstream	infile;
+	std::ofstream	outfile;
+	std::string		buffer;
+	size_t			pos;
+
+	infile.open(av[1]);
+	outfile.open(((std::string)av[1]).append(".replace").c_str());
+	if (infile.is_open() && outfile.is_open())
+	{
+		while (std::getline(infile, buffer))
+		{
+			while ((pos = buffer.find(s1)) != std::string::npos)
+			{
+				buffer.erase(pos, s1.length());
+				buffer.insert(pos, s2);
+			}
+			outfile << buffer << std::endl;
+		}
+	}
+	else
+	{
+		infile.close();
+		outfile.close();
+		return (std::cout << "error: can't open file" << std::endl, 1);
+	}
+	infile.close();
+	outfile.close();
+	return (0);
+}
