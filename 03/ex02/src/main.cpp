ef241d423bd5ae052e26040316030dd33dcaa029
matszx <coding@mcygan.anonaddy.me>
2024-12-09 15:13:28 +0100

    better main

--- 03/ex02/src/main.cpp
+++ 03/ex02/src/main.cpp
@@ -4,10 +4,17 @@
 int	main(void)
 {
 	ClapTrap a;
+	FragTrap tmp1("Alfred");
+	FragTrap tmp2(tmp1);
 	FragTrap b;
 
+	b = tmp2;
+
+	a.printStats();
+	b.printStats();
 	a.attack("FragTrap");
 	b.takeDamage(0);
+	b.beRepaired(30);
 	b.attack("ClapTrap");
 	a.takeDamage(30);
 	b.highFivesGuys();
