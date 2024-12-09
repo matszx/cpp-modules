1a7d042f9436d8a08a7379674ee1afefe632d069
matszx <coding@mcygan.anonaddy.me>
2024-12-09 13:31:28 +0100

    adjustments idk

--- 03/ex00/src/main.cpp
+++ 03/ex00/src/main.cpp
@@ -3,12 +3,15 @@
 int	main(void)
 {
 	ClapTrap a;
-	ClapTrap b("Alfred");
-	ClapTrap c(b);
+	ClapTrap tmp1("Alfred");
+	ClapTrap tmp2(tmp1);
+	ClapTrap b = tmp2;
 
+	a.printStats();
 	a.attack("Alfred");
 	b.takeDamage(0);
-	c.beRepaired(10);
+	b.beRepaired(10);
+	b.printStats();
 
 	return (0);
 }
