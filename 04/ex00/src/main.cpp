faef8c9037b8f7a912d33b76f1f89b1647e39d91
matszx <coding@mcygan.anonaddy.me>
2024-12-11 14:35:07 +0100

    created WrongAnimal and WrongCat classes

--- 04/ex00/src/main.cpp
+++ 04/ex00/src/main.cpp
@@ -1,21 +1,35 @@
-#include "../inc/Dog.hpp"
 #include "../inc/Cat.hpp"
+#include "../inc/Dog.hpp"
+#include "../inc/WrongCat.hpp"
 
 int main()
 {
-	const Animal* animal = new Animal();
-	const Animal* dog = new Dog();
-	const Animal* cat = new Cat();
-	
-	std::cout << dog->getType() << std::endl;
-	std::cout << cat->getType() << std::endl;
-	dog->makeSound();
-	cat->makeSound();
-	animal->makeSound();
+	{
+		const Animal* animal = new Animal();
+		const Animal* cat = new Cat();
+		const Animal* dog = new Dog();
+		
+		std::cout << cat->getType() << std::endl;
+		std::cout << dog->getType() << std::endl;
+		cat->makeSound();
+		dog->makeSound();
+		animal->makeSound();
 
-	delete animal;
-	delete dog;
-	delete cat;
+		delete animal;
+		delete cat;
+		delete dog;
+	}
+	std::cout << std::endl;
+	{
+		const WrongAnimal* animal = new WrongAnimal();
+		const WrongAnimal* cat = new WrongCat();
+		
+		std::cout << cat->getType() << std::endl;
+		cat->makeSound();
+		animal->makeSound();
 
+		delete animal;
+		delete cat;
+	}
 	return 0;
 }
