0fcb72e89536caf3c4f10119517790b9cb008b0a
matszx <coding@mcygan.anonaddy.me>
2026-01-13 00:41:34 +0100

    added eval tests

--- 07/ex00/inc/whatever.hpp
+++ 07/ex00/inc/whatever.hpp
@@ -1,18 +1,20 @@
 #pragma	once
 
-template <typename T> void swap(T a, T b)
+#include <iostream>
+
+template <typename T> void swap(T& a, T& b)
 {
 	T temp = a;
 	a = b;
 	b = temp;
 }
 
-template <typename T> T min(T a, T b)
+template <typename T> T min(const T& a, const T& b)
 {
 	return (a < b) ? a : b;
 }
 
-template <typename T> T max(T a, T b)
+template <typename T> T max(const T& a, const T& b)
 {
 	return (a > b) ? a : b;
 }
--- 07/ex00/src/main.cpp
+++ 07/ex00/src/main.cpp
@@ -1,24 +1,54 @@
-#include <iostream>
 #include "../inc/whatever.hpp"
 
+/* class Awesome
+{
+	private:
+		int _n;
+	public:
+		Awesome(void) : _n(0) {}
+		Awesome( int n ) : _n( n ) {}
+		Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+		bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+		bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+		bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+		bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+		bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+		bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+		int get_n() const { return _n; }
+};
 
-int main(void) {
-	std::cout << "----- pdf test -----" << std::endl << std::endl;
+std::ostream & operator<<(std::ostream & o, const Awesome &a)
+{
+	o << a.get_n();
+	return o;
+} */
+
+int main(void)
+{
 	int a = 2;
 	int b = 3;
 	
-	::swap( a, b );
+	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+
+	/* Awesome e(2), f(4);
+
+	std::cout << std::endl;
+	std::cout << e << " " << f << std::endl;
+	swap(e, f);
+	std::cout << e << " " << f << std::endl;
+	std::cout << max(e, f) << std::endl;
+	std::cout << min(e, f) << std::endl; */
 
 	return 0;
 }
