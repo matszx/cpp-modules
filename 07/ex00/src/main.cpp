cfdd7e59468e5651e30ed10e8cdcd4e1c4f31dfd
matszx <coding@mcygan.anonaddy.me>
2026-01-12 13:28:59 +0100

    ex00 done

---
+++ 07/ex00/src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "../inc/whatever.hpp"
+
+
+int main(void) {
+	std::cout << "----- pdf test -----" << std::endl << std::endl;
+	int a = 2;
+	int b = 3;
+	
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return 0;
+}
