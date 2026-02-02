c4a6cac4f9e50fdfb8cd5224bf803cd2785cd24a
matszx <coding@mcygan.anonaddy.me>
2026-02-02 15:26:34 +0100

    ex01 mostly done

---
+++ 08/ex01/inc/Span.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+#include <time.h>
+#include <exception>
+
+class Span
+{
+	private:
+		std::vector<int>	_vec;
+	public:
+		Span();
+		Span(unsigned int n);
+		Span(const Span& src);
+		~Span();
+		void			addNumber(int num);
+		unsigned int	shortestSpan();
+		unsigned int	longestSpan();
+		void			fillSpan();
+
+	class NoSpaceLeft: public std::exception
+	{
+		const char*	what() const throw();
+	};
+	class InsufficientElements: public std::exception
+	{
+		const char*	what() const throw();
+	};
+};
---
+++ 08/ex01/src/Span.cpp
@@ -0,0 +1,70 @@
+#include "../inc/Span.hpp"
+
+Span::Span()
+{
+	_vec.reserve(0);
+}
+
+Span::Span(unsigned int n)
+{
+	_vec.reserve(n);
+}
+
+Span::Span(const Span& src)
+{
+	*this = src;
+}
+
+Span::~Span() {}
+
+void			Span::addNumber(int num)
+{
+	if (_vec.size() == _vec.capacity())
+		throw Span::NoSpaceLeft();
+	_vec.push_back(num);
+}
+
+unsigned int	Span::shortestSpan()
+{
+	std::vector<int>	tmp = _vec;
+	int					diff, min;
+
+	if (_vec.size() < 2)
+		throw Span::InsufficientElements();
+	std::sort(tmp.begin(), tmp.end());
+	min = tmp[1] - tmp[0];
+	for (unsigned int i = 1; i < tmp.size() - 1; i++)
+	{
+		diff = tmp[i+1] - tmp[i];
+		if (min > diff)
+			min = diff;
+	}
+	return min;
+}
+
+unsigned int	Span::longestSpan()
+{
+	std::vector<int>	tmp = _vec;
+
+	if (_vec.size() < 2)
+		throw Span::InsufficientElements();
+	std::sort(tmp.begin(), tmp.end());
+	return tmp[tmp.size() - 1] - tmp[0];
+}
+
+void			Span::fillSpan()
+{
+	srand(time(NULL));
+	while (_vec.size() < _vec.capacity())
+		addNumber(rand());
+}
+
+const char* Span::NoSpaceLeft::what() const throw()
+{
+	return "NoSpaceLeft";
+}
+
+const char* Span::InsufficientElements::what() const throw()
+{
+	return "InsufficientElements";
+}
