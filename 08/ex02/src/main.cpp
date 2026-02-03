1346796f2e20df5475b7fcbf9fe53f7b1032e22f
matszx <coding@mcygan.anonaddy.me>
2026-02-03 17:54:24 +0100

    ex02 done

--- 08/ex02/src/main.cpp
+++ 08/ex02/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <algorithm>
 #include <list>
+#include <time.h>
 #include "../inc/MutantStack.hpp"
 
 int main()
@@ -27,23 +29,22 @@ int main()
 			++it;
 		}
 		std::stack<int> s(mstack);
-		return 0;
 	}
-	/* std::cout << std::endl << "----- PDF STD::LIST -----" << std::endl;
+	std::cout << std::endl << "----- PDF STD::LIST -----" << std::endl;
 	{
 		std::list<int> mstack;
 		mstack.push_back(5);
 		mstack.push_back(17);
-		std::cout << mstack.top() << std::endl;
-		mstack.pop();
+		std::cout << mstack.back() << std::endl;
+		mstack.pop_back();
 		std::cout << mstack.size() << std::endl;
 		mstack.push_back(3);
 		mstack.push_back(5);
 		mstack.push_back(737);
 		//[...]
 		mstack.push_back(0);
-		MutantStack<int>::iterator it = mstack.begin();
-		MutantStack<int>::iterator ite = mstack.end();
+		std::list<int>::iterator it = mstack.begin();
+		std::list<int>::iterator ite = mstack.end();
 		++it;
 		--it;
 		while (it != ite)
@@ -51,7 +52,49 @@ int main()
 			std::cout << *it << std::endl;
 			++it;
 		}
-		std::stack<int> s(mstack);
-		return 0;
-	} */
+		std::list<int> s(mstack);
+	}
+
+	MutantStack<int>	s;
+
+	srand(time(0));
+	for (int i = 0; i < 10; i++)
+		s.push(rand() % 100);
+
+	std::cout << std::endl << "----- ITERATING OVER STACK -----" << std::endl;
+	{
+		MutantStack<int>::iterator	it = s.begin();
+		while (it != s.end())
+			std::cout << *(it++) << " ";
+		std::cout << std::endl;
+		it = s.end();
+		it--;
+		while (it != s.begin())
+			std::cout << *(it--) << " ";
+		if (it == s.begin())
+			std::cout << *it << std::endl;
+	}
+	std::cout << std::endl << "----- STACK METHODS & MEMBERS -----" << std::endl;
+	{
+		MutantStack<int>	other;
+		other.push(50);
+
+		s.push(42);
+		std::cout << "size: " << s.size() << "   top elem: " << s.top() << std::endl;
+		s.pop();
+		std::cout << "size: " << s.size() << "   top elem: " << s.top() << std::endl;
+		//std::cout << "other.top(): " << other.top() << std::endl;
+		std::cout << std::boolalpha;
+		std::cout << "stack empty: " << s.empty() << std::endl;
+		std::cout << "s == other : " << (s == other) << std::endl;
+		std::cout << "s != other : " << (s != other) << std::endl;
+		std::cout << "s < other : " << (s < other) << std::endl;
+		std::cout << "s <= other : " << (s <= other) << std::endl;
+		std::cout << "s > other : " << (s > other) << std::endl;
+		std::cout << "s >= other : " << (s >= other) << std::endl;
+		while (s.size())
+			s.pop();
+		std::cout << "stack empty: " << s.empty() << std::endl;
+	}
+	return 0;
 }
